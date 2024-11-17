b36e00e1754c7330d9ffc34d88416231bdda3063
AJ ONeal <aj@therootcompany.com>
2024-11-16 18:06:01 -0700

    ref!(wallet): introduce reservation system and update tests

--- tests/wallet.js
+++ tests/wallet.js
@@ -3,10 +3,14 @@ import Wallet from "../wallet.js";
 import DashPhrase from "dashphrase";
 import DashHd from "dashhd";
 
+// empower illness dry increase season blind girl acid sustain ankle result smile
 // casino reveal crop open ordinary garment spy pizza clown exercise poem enjoy
 let phrase =
 	"own found reform rapid phrase change oxygen spend amazing hungry tenant claw";
 let salt = "";
+
+let nonRandomPoolSize = 1;
+let maxRounds = 3;
 
 async function test() {
 	let network = "testnet";
@@ -20,7 +24,7 @@ async function test() {
 	let seedBytes = await DashPhrase.toSeed(phrase, salt);
 	let walletKey = await DashHd.fromSeed(seedBytes, {
 		purpose: 44, // BIP-44 (default)
-		// coinType, // TODO fix and then unbreak tests
+		coinType,
 		versions,
 	});
 	let accountIndex = 0;
@@ -29,6 +33,7 @@ async function test() {
 		walletKey,
 		accountIndex,
 	);
+	let cjAccountInfo = accountInfo; // same for now
 
 	let xkeyInfo = await Wallet.rawGetUsageKey(
 		network,
@@ -36,84 +41,139 @@ async function test() {
 		accountIndex,
 		DashHd.RECEIVE,
 	);
-	let keyStates = await Wallet.getUnusedKeys(xkeyInfo.accountId, xkeyInfo, 3);
+	let keyStates = await Wallet.peekUnusedKeys(xkeyInfo, 3);
 
 	{
 		let firstAddress = keyStates[0].address;
 
-		let cjInfo0 = await Wallet.getCoinJoinAddressFor(accountInfo, firstAddress);
-		let round0 = cjInfo0.address; // for denomination
+		let cjLockedState0 = await Wallet.CJ.takeRoundKey(
+			cjAccountInfo,
+			firstAddress,
+			maxRounds,
+			xkeyInfo,
+			nonRandomPoolSize,
+		)
+			.then(function () {
+				throw new Error("expected error");
+			})
+			.catch(async function (err) {
+				if (err.code !== "E_NOT_COINJOIN_LOCKED") {
+					throw err;
+				}
+				let cjLockedState = await Wallet.CJ.takeDenominationKey(
+					cjAccountInfo,
+					firstAddress,
+					nonRandomPoolSize,
+				);
+				return cjLockedState;
+			});
+		let round0 = cjLockedState0.address; // for denomination
 		if (
-			round0 !== "yZMv92ph3iEdghotEHAcfeygrAdnFH85Jv" ||
-			cjInfo0.usage !== 2
+			round0 !== "yZ43umwfNDVCeBw8hh84hxdqxCpMosPsMk" ||
+			cjLockedState0.usage !== 2
 		) {
 			throw new Error(`cj round 0 broke`);
 		}
 
-		let cjInfo1 = await Wallet.getCoinJoinAddressFor(accountInfo, round0);
-		let round1 = cjInfo1.address;
+		let cjLockedState1 = await Wallet.CJ.takeRoundKey(
+			cjAccountInfo,
+			round0,
+			maxRounds,
+			xkeyInfo,
+		);
+		let round1 = cjLockedState1.address;
 		if (
-			round1 !== "yWyVWPioYWWVYzw9EHseW1R4bbMUWQ7a84" ||
-			cjInfo1.usage !== 3
+			round1 !== "yP8oJJcq2TfDC5G2RsyQV3SpJhE123vxkQ" ||
+			cjLockedState1.usage !== 3
 		) {
 			throw new Error(`cj round 1 broke`);
 		}
 
-		let cjInfo2 = await Wallet.getCoinJoinAddressFor(accountInfo, round1);
-		let round2 = cjInfo2.address;
+		let cjLockedState2 = await Wallet.CJ.takeRoundKey(
+			cjAccountInfo,
+			round1,
+			maxRounds,
+			xkeyInfo,
+		);
+		let round2 = cjLockedState2.address;
 		if (
-			round2 !== "yLzz6mDbcu9pNC8Kn1rRopSbJpRHqMv5c3" ||
-			cjInfo2.usage !== 4
+			round2 !== "yRKcWaWSzkUFHytQbLYsarmFUQCe4NPva1" ||
+			cjLockedState2.usage !== 4
 		) {
 			throw new Error(`cj round 2 broke`);
 		}
 	}
 
 	{
-		// mark this address as used and update related state
-		let keyState =
-			Wallet.Addresses._cache["yZMv92ph3iEdghotEHAcfeygrAdnFH85Jv"];
-		keyState.hasBeenUsed = true;
-
-		let cjUsageInfo = await Wallet.rawGetUsageKey(
-			network,
-			walletKey,
-			accountIndex,
-			Wallet.USAGE_COINJOIN,
-		);
-		let usageState = Wallet._getState(
-			cjUsageInfo.accountId,
-			Wallet.USAGE_COINJOIN,
-		);
-		Wallet.updateKeyState(usageState, keyState);
-		// console.log(Wallet.Addresses._cache);
-		//
-		//
-
 		let address2 = keyStates[1].address;
 
-		let cjInfo0 = await Wallet.getCoinJoinAddressFor(accountInfo, address2);
-		let round0 = cjInfo0.address; // for denomination
+		let cjLockedState0 = await Wallet.CJ.takeDenominationKey(
+			accountInfo,
+			address2,
+			nonRandomPoolSize,
+		);
+		let round0 = cjLockedState0.address; // for denomination
 
-		let cjInfo1 = await Wallet.getCoinJoinAddressFor(accountInfo, round0);
-		let round1 = cjInfo1.address;
+		let cjLockedState1 = await Wallet.CJ.takeRoundKey(
+			cjAccountInfo,
+			round0,
+			maxRounds,
+			xkeyInfo,
+		);
+		let round1 = cjLockedState1.address;
 
-		let cjInfo2 = await Wallet.getCoinJoinAddressFor(accountInfo, round1);
-		let round2 = cjInfo2.address;
+		let cjLockedState2 = await Wallet.CJ.takeRoundKey(
+			cjAccountInfo,
+			round1,
+			maxRounds,
+			xkeyInfo,
+		);
+		let round2 = cjLockedState2.address;
 		if (
-			round2 !== "yWVADA7QxPXA5XrGj5mqHxRY8eckiAumYZ" ||
-			cjInfo2.usage !== 4
+			round2 !== "yeUXmMCBmGbt7oBdV7fKzUivRBngRApaW7" ||
+			cjLockedState2.usage !== 4
 		) {
+			console.log(round2);
 			throw new Error(`cj round 2.2 broke`);
 		}
 
-		let maxRounds = 1;
-		let msg = "should not have exceeded max rounds";
+		let lowerRoundsToUnlock = 0;
+		let unlockedState = await Wallet.CJ.takeRoundKey(
+			cjAccountInfo,
+			round1,
+			lowerRoundsToUnlock,
+			xkeyInfo,
+		);
+
 		try {
-			void (await Wallet.getCoinJoinAddressFor(accountInfo, round1, maxRounds));
+			void (await Wallet.CJ.takeRoundKey(
+				cjAccountInfo,
+				unlockedState.address,
+				maxRounds,
+				xkeyInfo,
+			));
+			let msg = "should have failed to advance non-coinjoin-locked address";
 			throw new Error(msg);
 		} catch (e) {
-			if (e.code !== "E_COINJOIN_MAX_ROUNDS") {
+			/** @type {Error & { code: String }} */ //@ts-expect-error
+			let err = e;
+			if (err.code !== "E_NOT_COINJOIN_LOCKED") {
+				throw e;
+			}
+		}
+
+		try {
+			void (await Wallet.CJ.takeDenominationKey(
+				cjAccountInfo,
+				round2,
+				nonRandomPoolSize,
+			));
+			let msg = "should have failed to denominate coinjoin-locked address";
+			throw new Error(msg);
+		} catch (e) {
+			/** @type {Error & { code: String }} */ //@ts-expect-error
+			let err = e;
+			if (err.code !== "E_COINJOIN_IN_OTHER_WALLET") {
 				throw e;
 			}
 		}
--- wallet.js
+++ wallet.js
@@ -1,17 +1,17 @@
 // import DashPhrase from "dashphrase";
-// import DashHd from "dashhd";
-// import DashKeys from "dashkeys";
+import DashHd from "dashhd";
+import DashKeys from "dashkeys";
 
 ////@ts-expect-error
 //let DashPhrase = window.DashPhrase;
 
-/** @type {import('dashhd')} */
-//@ts-expect-error
-let DashHd = globalThis.DashHd || require("dashhd");
+///** @type {import('dashhd')} */
+////@ts-expect-error
+//let DashHd = globalThis.DashHd || require("dashhd");
 
-/** @type {import('dashkeys')} */
-//@ts-expect-error
-let DashKeys = globalThis.DashKeys || require("dashkeys");
+///** @type {import('dashkeys')} */
+////@ts-expect-error
+//let DashKeys = globalThis.DashKeys || require("dashkeys");
 
 ////@ts-expect-error
 //let DashTx = window.DashTx;
@@ -68,7 +68,7 @@ let DashKeys = globalThis.DashKeys || require("dashkeys");
 const USAGE_RECEIVE = DashHd.RECEIVE;
 const USAGE_CHANGE = DashHd.CHANGE;
 const USAGE_COINJOIN = 2;
-const ROUNDS_TARGET = 16;
+// const ROUNDS_TARGET = 16;
 
 const COINTYPE_DASH = 5;
 const COINTYPE_TESTNET = 1; // testnet (for all coins)
@@ -164,10 +164,10 @@ Wallet._walletStates = {
 
 /**
  * @param {AccountID} accountId
- * @param {Usage} usage
+ * @param {Usage} usage - index (0 receive, 1 change, 2+ coinjoin)
  * @returns {WalletStateSparse}
  */
-Wallet._getState = function (accountId, usage) {
+Wallet._getUsageState = function (accountId, usage) {
 	let accountState = Wallet._walletStates[accountId];
 	if (!accountState) {
 		accountState = [];
@@ -250,17 +250,96 @@ Wallet.rawGetUsageKey = async function (
 	};
 };
 
+Wallet.CJ = {};
+
 /**
- * @param {AccountInfo} accountInfo
- * @param {Address} address
- * @param {Number} [maxRounds=ROUNDS_TARGET] - typically 16
+ * Reserves an address for denominating a coin
+ * @param {AccountInfo} cjAccountInfo
+ * @param {Address} [address]
+ * @param {Number} [denominationPoolSize] - 1: select next denomination key, 1+: select denomination key randomly from pool
  * @returns {Promise<KeyState>}
  */
-Wallet.getCoinJoinAddressFor = async function (
-	accountInfo,
+Wallet.CJ.takeDenominationKey = async function (
+	cjAccountInfo,
 	address,
-	maxRounds = ROUNDS_TARGET,
+	denominationPoolSize,
 ) {
+	if (address) {
+		let keyState = Wallet.Addresses._cache[address];
+		let usageRound = keyState.usage - USAGE_COINJOIN;
+
+		let mustDenominate = usageRound < 0;
+		if (!mustDenominate) {
+			let msg = `'${address}' is already in round ${usageRound} of CoinJoin in wallet ${keyState.walletId}'s account ${keyState.accountId}`;
+			let err = new Error(msg);
+			Object.assign(err, { code: "E_COINJOIN_IN_OTHER_WALLET" });
+			throw err;
+		}
+	}
+
+	let usageKey = await Wallet._getXPrv(cjAccountInfo, USAGE_COINJOIN);
+	let xkeyInfo = {
+		network: cjAccountInfo.network,
+		walletId: cjAccountInfo.walletId,
+		accountId: cjAccountInfo.accountId,
+		// jshint bitwise: false
+		accountIndex: cjAccountInfo.accountKey.index & 0x7fffffff, // (for "hardening")
+		usageKey: usageKey,
+	};
+
+	let maxRoundsForReal = 42; // anything beyond this would be meaningless
+	let maxTriesCount = 3; // anything more than 2 is excessive
+	let poolSize = 0;
+	if (denominationPoolSize === 1) {
+		poolSize = maxTriesCount;
+	} else if (poolSize < 4) {
+		poolSize = 4;
+	}
+	let keyStates = await Wallet.takeUnusedKeys(
+		xkeyInfo,
+		maxTriesCount,
+		poolSize,
+	);
+	let lastError = new Error("none");
+	for (let keyState of keyStates) {
+		try {
+			for (
+				let usage = USAGE_COINJOIN + 1;
+				usage <= maxRoundsForReal;
+				usage += 1
+			) {
+				let usageKey = await Wallet._getXPrv(cjAccountInfo, usage);
+				void (await usageKey.deriveAddress(keyState.index));
+			}
+		} catch (e) {
+			//@ts-expect-error // it's an error bro
+			lastError = e;
+			continue;
+		}
+
+		return keyState;
+	}
+	lastError.message = `failure to verify coinjoin round addresses for '${address}': ${lastError.message}`;
+	throw lastError;
+};
+
+/**
+ * Reserves an address key for a CoinJoin round
+ * @param {AccountInfo} cjAccountInfo
+ * @param {Address} address
+ * @param {Number} maxRounds - typically 16
+ * @param {XKeyInfo} destinationXPrvInfo - RECEIVE usage xprv
+ * @param {Number} [destinationPoolSize] - 1: select first destination key, 1+: select destination key randomly from pool
+ * @returns {Promise<KeyState>}
+ */
+Wallet.CJ.takeRoundKey = async function (
+	cjAccountInfo,
+	address,
+	maxRounds,
+	destinationXPrvInfo,
+	destinationPoolSize,
+) {
+	/** @type {KeyState} */
 	let roundInfo = Wallet.Addresses._cache[address];
 
 	// Implementation decisions (not decided by spec)
@@ -278,79 +357,46 @@ Wallet.getCoinJoinAddressFor = async function (
 
 	let usageRound = roundInfo.usage - USAGE_COINJOIN;
 	if (usageRound >= maxRounds) {
-		let msg = `${address} has already been through ${maxRounds} CoinJoin rounds`;
-		let err = new Error(msg);
-		Object.assign(err, { code: "E_COINJOIN_MAX_ROUNDS" });
-		throw err;
+		// let msg = `${address} has already been through ${maxRounds} CoinJoin rounds`;
+		// let err = new Error(msg);
+		// Object.assign(err, { code: "E_COINJOIN_MAX_ROUNDS" });
+		// throw err;
+		// if (err.code !== "E_COINJOIN_MAX_ROUNDS") {
+		// 	throw err;
+		// }
+
+		let keyState = await Wallet.takeUnusedKey(
+			destinationXPrvInfo,
+			destinationPoolSize,
+		);
+		return keyState;
 	}
 
 	let mustDenominate = usageRound < 0;
 	if (mustDenominate) {
-		/** @type {import('dashhd').HDXKey} */
-		let usageKey = await Wallet._getXPrv(accountInfo, USAGE_COINJOIN);
-		let xkeyInfo = {
-			network: accountInfo.network,
-			walletId: accountInfo.walletId,
-			accountId: accountInfo.accountId,
-			// jshint bitwise: false
-			accountIndex: accountInfo.accountKey.index & 0x7fffffff, // (for "hardening")
-			usageKey: usageKey,
-		};
-
-		let maxRoundsForReal = 42; // anything beyond this would be meaningless
-		let maxTriesCount = 3; // anything more than 2 is excessive
-		let keyStates = await Wallet.getUnusedKeys(
-			accountInfo.accountId,
-			xkeyInfo,
-			maxTriesCount,
-		);
-		for (let keyState of keyStates) {
-			try {
-				for (
-					let usage = USAGE_COINJOIN + 1;
-					usage <= maxRoundsForReal;
-					usage += 1
-				) {
-					/** @type {import('dashhd').HDXKey} */
-					let usageKey = await Wallet._getXPrv(accountInfo, usage);
-					void (await usageKey.deriveAddress(keyState.index));
-				}
-			} catch (e) {
-				continue;
-			}
-
-			return keyState;
-		}
-		throw new Error(`sanity fail: the universe has run out of entropy`);
+		let msg = `${address} has not been denominated and/or locked for coinjoin`;
+		let err = new Error(msg);
+		Object.assign(err, { code: "E_NOT_COINJOIN_LOCKED" });
+		throw err;
 	}
 
-	{
-		if (roundInfo.accountId !== accountInfo.accountId) {
-			let msg = `'${address}' is already in round ${usageRound} of CoinJoin in wallet ${roundInfo.walletId}'s account ${roundInfo.accountId}`;
-			let err = new Error(msg);
-			Object.assign(err, { code: "E_COINJOIN_IN_OTHER_WALLET" });
-			throw err;
-		}
+	let nextUsage = roundInfo.usage + 1;
+	let usageKey = await Wallet._getXPrv(cjAccountInfo, nextUsage);
+	let xkeyInfo = {
+		network: roundInfo.network,
+		walletId: roundInfo.walletId,
+		accountId: roundInfo.accountId,
+		// jshint bitwise: false
+		accountIndex: cjAccountInfo.accountKey.index & 0x7fffffff, // (for "hardening")
+		usageKey: usageKey,
+	};
+	let addressKey = await usageKey.deriveAddress(roundInfo.index);
 
-		let nextUsage = roundInfo.usage + 1;
-		// /** @type {import('dashhd').HDXKey} */
-		let usageKey = await Wallet._getXPrv(accountInfo, nextUsage);
-		let xkeyInfo = {
-			network: accountInfo.network,
-			walletId: accountInfo.walletId,
-			accountId: accountInfo.accountId,
-			// jshint bitwise: false
-			accountIndex: accountInfo.accountKey.index & 0x7fffffff, // (for "hardening")
-			usageKey: usageKey,
-		};
-		let addressKey = await usageKey.deriveAddress(roundInfo.index);
-
-		// note: we can skip the failed index check here
-		//       because it is done when denominating
-		let keyInfo = await Wallet._rawGetKeyInfo(xkeyInfo, addressKey);
-		let keyState = Wallet._mergeKeyState(keyInfo);
-		return keyState;
-	}
+	// note: we can skip the failed index check here
+	//       because it is done when denominating
+	let keyInfo = await Wallet._rawGetKeyInfo(xkeyInfo, addressKey);
+	let keyState = Wallet._mergeKeyState(keyInfo);
+	return keyState;
 };
 
 /**
@@ -368,6 +414,7 @@ Wallet._mergeKeyState = function (keyInfo) {
 /**
  * @param {AccountInfo} accountInfo
  * @param {Number} usage
+ * @returns {Promise<import('dashhd').HDXKey>}
  */
 Wallet._getXPrv = async function (accountInfo, usage) {
 	let xprvKeys = Wallet._coinjoinXPrvs[accountInfo.accountId];
@@ -386,24 +433,73 @@ Wallet._getXPrv = async function (accountInfo, usage) {
 };
 
 /**
- * Get (offline-cached) list of unused receive addresses
- * @param {AccountID} accountId
+ * Take unused keys and mark them as reserved
+ * @param {XKeyInfo} xkeyInfo
+ * @param {Number} [poolSize] - 1: select first key, 1+: select key randomly from pool
+ */
+Wallet.takeUnusedKey = async function (xkeyInfo, poolSize) {
+	let keyStates = await Wallet.takeUnusedKeys(xkeyInfo, 1, poolSize);
+	let keyState = keyStates[0];
+
+	keyState.reservedAt = Date.now();
+	Wallet.updateKeyState(keyState);
+
+	return keyState;
+};
+
+/**
+ * Take unused keys, marking them as reserved
+ * @param {XKeyInfo} xkeyInfo
+ * @param {Number} count - how many to reserve
+ * @param {Number} [poolSize] - how many to select from (default: 10*count)
+ */
+Wallet.takeUnusedKeys = async function (xkeyInfo, count, poolSize) {
+	if (!poolSize) {
+		poolSize = 10 * count;
+	}
+
+	Wallet._reserveLock = Wallet._reserveLock.then(async function () {
+		let _keyStates = await Wallet.peekUnusedKeys(xkeyInfo, poolSize);
+		if (poolSize !== count) {
+			void knuthShuffle(_keyStates);
+		}
+
+		_keyStates = _keyStates.slice(0, count);
+		for (let keyState of _keyStates) {
+			keyState.reservedAt = Date.now();
+			Wallet.updateKeyState(keyState);
+		}
+
+		return _keyStates;
+	});
+
+	let keyStates = await Wallet._reserveLock;
+	return keyStates;
+};
+/** @type {Promise<Array<KeyState>>} */ //@ts-expect-error
+Wallet._reserveLock = Promise.resolve(null);
+
+/**
+ * Get (offline-cached) list of unused receive addresses, without reserving them
  * @param {XKeyInfo} xkeyInfo
  * @param {Number} [count=100] - get addresses from offset to offset + N-1, ex: 0-99
  */
-Wallet.getUnusedKeys = async function (accountId, xkeyInfo, count = 100) {
+Wallet.peekUnusedKeys = async function (xkeyInfo, count = 100) {
 	// TODO OT Quick Note: TODO
 	// On lead:
 	// - first check existing utxos again to see if they've been spent
 	// - next check if unused coins now have money
 	// - optimize for happy path, offline path, then online path
 
-	if (xkeyInfo.accountId !== accountId) {
-		throw new Error(
-			`xkeyInfo.accountId must be set and equal to the desired accountId`,
-		);
-	}
-	let usageState = Wallet._getState(accountId, xkeyInfo.usageKey.index);
+	// if (xkeyInfo.accountId !== accountId) {
+	// 	throw new Error(
+	// 		`xkeyInfo.accountId must be set and equal to the desired accountId`,
+	// 	);
+	// }
+	let usageState = Wallet._getUsageState(
+		xkeyInfo.accountId,
+		xkeyInfo.usageKey.index,
+	);
 	let indexes = Object.keys(usageState.unused);
 
 	let n = indexes.length;
@@ -411,7 +507,12 @@ Wallet.getUnusedKeys = async function (accountId, xkeyInfo, count = 100) {
 	let limit = count;
 
 	for (; n < count; ) {
-		usageState = await Wallet.getKeyStates(accountId, xkeyInfo, offset, limit);
+		usageState = await Wallet.getKeyStates(
+			xkeyInfo.accountId,
+			xkeyInfo,
+			offset,
+			limit,
+		);
 		indexes = Object.keys(usageState.unused);
 		n = indexes.length;
 		offset += limit;
@@ -458,21 +559,28 @@ Wallet.getKeyStates = async function (
 ) {
 	let keyInfos = await Wallet.rawGetKeyInfos(xkeyInfo, offset, limit);
 
-	let usageState = Wallet._getState(accountId, xkeyInfo.usageKey.index);
-
+	let usageState = Wallet._getUsageState(accountId, xkeyInfo.usageKey.index);
 	for (let keyInfo of keyInfos) {
 		let keyState = Wallet._mergeKeyState(keyInfo);
-		void Wallet.updateKeyState(usageState, keyState);
+		void Wallet._updateKeyState(usageState, keyState);
 	}
 
 	return usageState;
 };
 
 /**
+ * @param {KeyState} keyState
+ */
+Wallet.updateKeyState = function (keyState) {
+	let usageState = Wallet._getUsageState(keyState.accountId, keyState.usage);
+	void Wallet._updateKeyState(usageState, keyState);
+};
+
+/**
  * @param {WalletStateSparse} usageState
  * @param {KeyState} keyState
  */
-Wallet.updateKeyState = function (usageState, keyState) {
+Wallet._updateKeyState = function (usageState, keyState) {
 	let caches = Wallet._caches[keyState.network];
 	caches.keysByAddress[keyState.address] = keyState;
 
@@ -673,8 +781,8 @@ Wallet.Addresses.setDeltas = async function (deltas) {
 	}
 	Object.assign(keyState, keyStateMini);
 
-	let usageState = Wallet._getState(keyState.accountId, keyState.index);
-	Wallet.updateKeyState(usageState, keyState);
+	let usageState = Wallet._getUsageState(keyState.accountId, keyState.usage);
+	Wallet._updateKeyState(usageState, keyState);
 
 	Wallet.Addresses.set(address, keyStateMini);
 };
@@ -716,7 +824,7 @@ Wallet.Addresses.all = async function () {
 	let results = {};
 	for (let i = 0; i < localStorage.length; i += 1) {
 		if (i % 10000 === 0) {
-			await sleep(10);
+			await thaw(10); // let event loop breathe
 		}
 		let key = localStorage.key(i);
 		if (!key) {
@@ -746,9 +854,35 @@ Wallet.Addresses.all = async function () {
 };
 
 /**
+ * @template T
+ * @param {Array<T>} array
+ * @returns {Array<T>}
+ */
+function knuthShuffle(array) {
+	let currentIndex = array.length;
+	let temporaryValue;
+
+	// While there remain elements to shuffle...
+	while (currentIndex > 0) {
+		// Pick a remaining element...
+		let randomIndex = Math.random() * currentIndex;
+		randomIndex = Math.floor(randomIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+/**
+ * Sleep, but named as to indicate we're using this to prevent from freezing up the main thread / event loop
  * @param {Number} ms
  */
-async function sleep(ms) {
+async function thaw(ms) {
 	return await new Promise(function (resolve) {
 		setTimeout(resolve, ms);
 	});
